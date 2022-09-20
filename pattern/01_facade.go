8ae4cf295d0c1b8d5ed0d228b08cd82a5e818ce9
alex <banchenko.a2@wb.ru>
2022-09-20 13:02:36 +0300

    init

--- pattern/01_facade.go
+++ pattern/01_facade.go
@@ -5,3 +5,60 @@ package pattern
 Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Facade_pattern
 */
+
+import (
+	"strings"
+)
+
+// NewMan creates man.
+func NewMan() *Man {
+	return &Man{
+		house: &House{},
+		tree:  &Tree{},
+		child: &Child{},
+	}
+}
+
+// Man implements man and facade.
+type Man struct {
+	house *House
+	tree  *Tree
+	child *Child
+}
+
+// Todo returns that man must do.
+func (m *Man) Todo() string {
+	result := []string{
+		m.house.Build(),
+		m.tree.Grow(),
+		m.child.Born(),
+	}
+	return strings.Join(result, "\n")
+}
+
+// House implements a subsystem "House"
+type House struct {
+}
+
+// Build implementation.
+func (h *House) Build() string {
+	return "Build house"
+}
+
+// Tree implements a subsystem "Tree"
+type Tree struct {
+}
+
+// Grow implementation.
+func (t *Tree) Grow() string {
+	return "Tree grow"
+}
+
+// Child implements a subsystem "Child"
+type Child struct {
+}
+
+// Born implementation.
+func (c *Child) Born() string {
+	return "Child born"
+}
--- pattern/06_factory_method.go
+++ pattern/06_factory_method.go
@@ -5,3 +5,83 @@ package pattern
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Factory_method_pattern
 */
+
+import (
+	"log"
+)
+
+// action helps clients to find out available actions.
+type action string
+
+const (
+	A action = "A"
+	B action = "B"
+	C action = "C"
+)
+
+// Creator provides a factory interface.
+type Creator interface {
+	CreateProduct(action action) ProductFM // Factory Method
+}
+
+// ProductFM provides a product interface.
+// All products returned by factory must provide a single interface.
+type ProductFM interface {
+	Use() string // Every product should be usable
+}
+
+// ConcreteCreator implements Creator interface.
+type ConcreteCreator struct{}
+
+// NewCreator is the ConcreteCreator constructor.
+func NewCreator() Creator {
+	return &ConcreteCreator{}
+}
+
+// CreateProduct is a Factory Method.
+func (p *ConcreteCreator) CreateProduct(action action) ProductFM {
+	var product ProductFM
+
+	switch action {
+	case A:
+		product = &ConcreteProductA{string(action)}
+	case B:
+		product = &ConcreteProductB{string(action)}
+	case C:
+		product = &ConcreteProductC{string(action)}
+	default:
+		log.Fatalln("Unknown Action")
+	}
+
+	return product
+}
+
+// ConcreteProductA implements product "A".
+type ConcreteProductA struct {
+	action string
+}
+
+// Use returns product action.
+func (p *ConcreteProductA) Use() string {
+	return p.action
+}
+
+// ConcreteProductB implements product "B".
+type ConcreteProductB struct {
+	action string
+}
+
+// Use returns product action.
+func (p *ConcreteProductB) Use() string {
+	return p.action
+}
+
+// ConcreteProductC implements product "C".
+type ConcreteProductC struct {
+	action string
+}
+
+// Use returns product action.
+func (p *ConcreteProductC) Use() string {
+	return p.action
+}
